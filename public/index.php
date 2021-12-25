92ea30c2a7d24eb48fda903c098f4dc5cf7993df
PetarBelberov <petar_belberov@gmx.com>
2021-12-25 20:04:43 +0200

    add: index.php front controller(base url function), Student controller and Student model

---
+++ public/index.php
@@ -0,0 +1,19 @@
+<?php
+
+
+require dirname(__DIR__).'./vendor/autoload.php';
+
+define('BASE_URL', baseURL());
+
+function baseURL(){
+    if(isset($_SERVER['HTTPS'])){
+        $protocol = ($_SERVER['HTTPS'] && $_SERVER['HTTPS'] != "off") ? "https" : "http";
+    }
+    else{
+        $protocol = 'http';
+    }
+    return $protocol . "://" . $_SERVER['HTTP_HOST'];
+}
+
+$student = new StudentController();
+$student->index();
---
+++ src/Controllers/StudentController.php
@@ -0,0 +1,16 @@
+<?php 
+// namespace Jd\Sap\Controllers;
+
+// use Jd\Sap\Models\Student;
+
+class StudentController
+{
+    function __construct() {
+        $this->student = new Student();
+    }
+    
+    public function index() {
+        
+    }
+}
+?>
